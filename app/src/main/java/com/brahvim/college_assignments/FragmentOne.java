6879d7abeac508f156f352e923f5a79c5fd726de
Brahvim <69293652+Brahvim@users.noreply.github.com>
2024-11-07 08:03:20 +0530

    Just how I like to format. Sorry I'm addicted.
    Signed-off-by: Brahvim <69293652+Brahvim@users.noreply.github.com>

--- app/src/main/java/com/brahvim/college_assignments/FragmentOne.java
+++ app/src/main/java/com/brahvim/college_assignments/FragmentOne.java
@@ -2,7 +2,6 @@ package com.brahvim.college_assignments;
 
 import android.app.Fragment;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -28,7 +27,7 @@ public class FragmentOne extends Fragment {
 	}
 
 	@Override
-	public void onViewCreated(final View p_view, @Nullable final Bundle p_saveState) {
+	public void onViewCreated(final View p_view, final Bundle p_saveState) {
 		this.binding.buttonSwitchFragmentFragmentOne.setOnClickListener(p_viewOnClick -> {
 			super
 			  .getFragmentManager()
